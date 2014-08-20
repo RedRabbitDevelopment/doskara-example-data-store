51f1bc9ceb7daf3aeeb8e94b996b3a7392824aef
Nathan Tate <nathantate88@gmail.com>
2014-08-19 20:13:49 -0600

    Saved

--- server.js
+++ server.js
@@ -1,53 +1,24 @@
 
-var doskara, Server, Q;
+var Q, _;
 Q = require('q');
-doskara = require('doskara');
 _ = require('lodash');
-doskara.init(require('./Doskara.json'));
+var config = require('./Doskara.json');
 
-var server = new doskara.Server((function() {
-  var store, cbs, id;
-    
-  store = [
+var server = require('socket.io').listen(config.ports.main, function() {
+  console.log('listening');
+});
+
+io.sockets.on('connection', function(socket) {
+  var store = [
     'booya',
     'gee',
     'one'
   ];
-  cbs = {};
-  id = 0;
-
-  return {
-    getAll: function() {
-      console.log('getting all', store);
-      return store;
-    },
-    setBeforeSave: function(callback) {
-      console.log('set before save');
-      id++;
-      console.log('before save', callback.toString());
-      cbs[id] = callback;
-      return cbs;
-    },
-    clearBeforeSave: function(id) {
-      console.log('clear before save');
-      delete cbs[id];
-    },
-    save: function(data) {
-      console.log('saving', data, cbs);
-      return Q.all(_.map(cbs, function(cb) {
-        return cb(data);
-      })).then(function(results) {
-        console.log('got through');
-        if(-1 !== results.indexOf(false) || true) {
-          store.push(data);
-          console.log('oh man', store);
-          return true;
-        };
-      });
-    }
-  };
-})());
-
-server.listen(doskara.ports.main).then(function() {
-  console.log('listening');
+  socket.on('getAll', function(ignore, fn) {
+    console.log('getting all', store);
+    fn(store);
+  });
+  socket.on('save', function(data, fn) {
+    store.push(data);
+  });
 });
